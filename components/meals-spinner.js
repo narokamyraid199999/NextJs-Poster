b840e8f4833968d68191a7a1e15c3efad35056c4
narokamyraid199999 <143643793+narokamyraid199999@users.noreply.github.com>
2025-08-26 16:31:47 -0700

    fear: wrap meals grid with Suspense loader

---
+++ components/meals-spinner.js
@@ -0,0 +1,7 @@
+export default function MealsSpinner() {
+  return (
+    <div className="w-full flex items-center justify-center">
+      <div className="w-20 h-20 border-t-2 border-b-2 border-white  rounded-full animate-spin"></div>
+    </div>
+  );
+}
